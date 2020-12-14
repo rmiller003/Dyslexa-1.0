ed4dfc59c141a5bc6c133f31611b80a4d9646426
rmill <rmiller003@gmail.com>
2020-12-13 22:36:29 -0500

    Done

--- dyslexa.py
+++ dyslexa.py
@@ -14,16 +14,32 @@ layout =   [ [sg.Text('Enter your Command'), sg.InputText()],
 # Create the Window
 window = sg.Window('Welcome to Dyslexa', layout)
 # Event Loop to process "events"
+
+import pyttsx3
+engine = pyttsx3.init()
+
+
 while True:
     event, values = window.read()
-    if event in (sg.WIN_CLOSED, 'Cancel'):
+    if event in (None, 'Cancel'):
         break
     try:
-        res = client.query(values[0])
-        wolfram_res = next(res.results).text
-        sg.Popup(wolfram_res)
+        wiki_res = wikipedia.summary(values[0], sentences=2)
+        wolfram_res = next(client.query(values[0]).results).textengine.say(wolfram_res)
+        sg.PopupNonBlocking("Wolfram Result: "+wolfram_res,"Wikipedia Result: "+wiki_res)
+    except wikipedia.exceptions.DisambiguationError:
+        wolfram_res = next(client.query(values[0]).results).text
+        engine.say(wolfram_res)
+        sg.PopupNonBlocking(wolfram_res)
+    except wikipedia.exceptions.PageError:
+        wolfram_res = next(client.query(values[0]).results).text
+        engine.say(wolfram_res)
+        sg.PopupNonBlocking(wolfram_res)
     except:
         wiki_res = wikipedia.summary(values[0], sentences=2)
-        sg.Popup(wiki_res)
+        engine.say(wiki_res)
+        sg.PopupNonBlocking(wiki_res)
+
+    engine.runAndWait()
 
 window.close()
